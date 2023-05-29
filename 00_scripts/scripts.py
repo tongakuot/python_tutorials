0b8d1ec3e4a624c78eb31cb9ba9e6a9e18390d5e
tongakuot <tongakuot@icloud.com>
2023-05-29 18:11:31 -0400

    added a new tutorial

--- 00_scripts/scripts.py
+++ 00_scripts/scripts.py
@@ -1,13 +1,13 @@
-def tweak_ss_census_data(df):
+def tweak_ss_census(df):
     return(df
           [cols]
           .rename(columns = cols_names)
-          .query('~age_cat.isna()')
-          .assign(gender = lambda x:x['gender'].str.split('\s+').str[1],
-                 age_cat = lambda x:x['age_cat'].replace(new_age_cats),
-                 population = lambda x:x['population'].astype('int')
+          .query('~age_cat.isna() & gender != "Total" & age_cat != "Total"')
+          .assign(gender = lambda df_: df_['gender'].str.split('\s+').str[1],
+                 age_cat = lambda df_: df_['age_cat'].replace(new_age_cats),
+                 population = lambda df_: df_['population'].astype('int')
           )
-          .query('gender != "Total" & age_cat != "Total"'
+          # .query('gender != "Total" & age_cat != "Total"'
           .groupby(['state', 'gender', 'age_cat'])['population']
           .sum()
           .reset_index()
